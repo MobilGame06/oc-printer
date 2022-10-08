3670d41f8d5b33e8af7b7d6cfbbc6743f6245d1d
Mobil Game 06 <75528709+MobilGame06@users.noreply.github.com>
2022-10-08 20:22:28 +0200

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -13,6 +13,7 @@ os.execute("wget 'https://raw.githubusercontent.com/MobilGame06/oc-printer/main/
 elseif input == "2" then
 os.execute("wget 'https://raw.githubusercontent.com/MobilGame06/oc-printer/main/library.lua' /lib/netprinter.lua")
 os.execute("wget 'https://raw.githubusercontent.com/MobilGame06/oc-printer/main/luaword.lua' /home/luaword.lua")
+os.execute("wget 'https://raw.githubusercontent.com/OpenPrograms/gamax92-Programs/master/vcomponent/vcomponent.lua' 
 end 
 
 term.clear()
